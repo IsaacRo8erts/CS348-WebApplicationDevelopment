011f11c9e5666f70d7a1c9479c70ed326d2a39d7
isaacroberts <isaac.roberts10@hotmail.co.uk>
2020-11-06 20:52:33 +0000

    seeder variables
    Signed-off-by: isaacroberts <isaac.roberts10@hotmail.co.uk>

---
+++ database/seeders/ProfileTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Profile;
+
+class ProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $p1 = new Profile;
+
+        $p1->pNum = 1;
+        $p1->firstName = "Matt";
+        $p1->surname = "Thomas" ;
+        $p1->age = 25;
+        $p1->favouriteTeam = "Bulls";
+        $p1->email = "matt.thomas@gmail.com";
+
+    }
+}
