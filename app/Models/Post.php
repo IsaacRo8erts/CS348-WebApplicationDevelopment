6ccab9dd384d22fbf9380d338b9b5a23accffd2b
isaacroberts <isaac.roberts10@hotmail.co.uk>
2020-11-07 02:45:05 +0000

    relations
    Signed-off-by: isaacroberts <isaac.roberts10@hotmail.co.uk>

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function profile()
+    {
+        return $this->belongsTo('App\Models\Profile');
+    }
 }
