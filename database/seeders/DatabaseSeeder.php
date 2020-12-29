18366f023b248400abd0631f1b536624d90223df
isaacroberts <isaac.roberts10@hotmail.co.uk>
2020-12-29 15:45:31 +0000

    Signed-off-by: isaacroberts <isaac.roberts10@hotmail.co.uk>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProfileTableSeeder::class,
+            UserTableSeeder::class,
             PostTableSeeder::class,
             CommentTableSeeder::class
         ]);
