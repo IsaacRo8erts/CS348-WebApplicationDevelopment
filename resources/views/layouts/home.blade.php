55c3fe670dab3288e88e9fcad97dc908e9b865de
isaacroberts <isaac.roberts10@hotmail.co.uk>
2020-12-30 22:31:53 +0000

    Signed-off-by: isaacroberts <isaac.roberts10@hotmail.co.uk>

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,5 +1,7 @@
+<!doctype html>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<html lang="en>
 
 <p class="text-calibri">Basketball Forum<img src="{{ URL::asset('images/nba.png') }}" width="200" height="50"></p>
 
