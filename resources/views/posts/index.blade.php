6d11f4090974724ed192926b709bc9f12c5c5a80
isaacroberts <isaac.roberts10@hotmail.co.uk>
2021-01-01 17:02:39 +0000

    WORKING BEFORWE COMMENTS
    Signed-off-by: isaacroberts <isaac.roberts10@hotmail.co.uk>

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,8 +7,6 @@
 
 @section('title', 'Posts')
 
-
-
 @section('content')
     <p>A list of the Basketball Forum posts:</p>
     <ul>
@@ -17,7 +15,7 @@
             $user = User::find($post->user_id);
         @endphp
             <li><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }} </a></li>
-            <a href="{{ route('users.show', ['id' => $user->id]) }}">{{ $user->name }} </a>
+            Posted by: <a href="{{ route('users.show', ['id' => $user->id]) }}">{{ $user->name }} </a>
         @endforeach
     </ul>
     <a href="{{ route('posts.create' )}}">Create Post</a>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,21 @@
+@php
+    use App\Models\User;
+@endphp
 @extends('layouts.app')
 
 @section('title', 'Post Details')
 
 @section('content')
     <ul>
-        <li>User ID: {{ $post->user_id }}</li>
-        <li>Title: {{ $post->title }}</li>
-        <li>Content: {{ $post->content }}</li>
+        @php
+            $user = User::find($post->user_id);
+        @endphp
+        User ID: {{ $post->user_id }}
+        <p>Name: {{ $user->name }}</p>
+
+        <p><li>Title: {{ $post->title }}</li>
+        <li>Content: {{ $post->content }}</li></p>
     </ul>
+
+    <button onclick="location.href='{{ route('register' )}}'">Add Comment</button>
 @endsection
